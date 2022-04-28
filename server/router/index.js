6733afb6ba053ba3adf0e7dbd7e7675056f2674a
Vladislav <dreamervt8@gmail.com>
2022-04-28 23:46:38 +0600

    services init

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const Router = require("express").Router;
+const userController = require("../controllers/user-controller");
 
 const router = new Router();
 
-router.post("/registration");
-router.post("/login");
-router.post("/logout");
-router.get("/activate/:link"); // Активация аккаунта по ссылке, которая придет на почту
-router.get("/refresh"); // Перезапись access token в случае истечения срока жизни токена
-router.get("/users");
+router.post("/registration", userController.registration);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.get("/activate/:link", userController.activate); // Активация аккаунта по ссылке, которая придет на почту
+router.get("/refresh", userController.refresh); // Перезапись access token в случае истечения срока жизни токена
+router.get("/users", userController.getUsers);
 
 module.exports = router;
