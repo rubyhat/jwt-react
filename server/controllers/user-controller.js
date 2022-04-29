508e6fdf2d0a3c507a47136d5ec4bb1113f66264
Vladislav <dreamervt8@gmail.com>
2022-04-29 10:49:16 +0600

    user-controller registration upd

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,7 +1,22 @@
+const userModel = require("../models/user-model");
+const userService = require("../service/user-service");
+
 class UserController {
   async registration(req, res, next) {
     try {
-    } catch (error) {}
+      const { email, password } = req.body; // достаем данные из запроса
+      const userData = await userService.registration(email, password); // Регистрируем юзера
+
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      }); // Сохраняем рефреш токен в куки и важный флаг, httpOnly защищает куки от извлечения в браузере, например при помощи js
+      // также если используется https то нужно добавить флаг secure:true
+
+      return res.json(userData);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async login(req, res, next) {
@@ -26,7 +41,7 @@ class UserController {
 
   async getUsers(req, res, next) {
     try {
-      res.json(["1", "2"]);
+      res.json("123");
     } catch (error) {}
   }
 }
